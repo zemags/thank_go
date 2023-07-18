98e0e20030060d5ef543d1a6901a995200127a65
zemags <azmt2008@gmail.com>
2023-07-18 21:26:55 +0300

    Add fundamentals

--- 1-fundamentals/main.go
+++ 1-fundamentals/main.go
@@ -3,7 +3,10 @@ package main
 import (
 	"fmt"
 	"math"
+	"math/rand"
+	"strings"
 	"time"
+	"unicode"
 )
 
 func printTime() {
@@ -43,9 +46,83 @@ func lang() {
 	fmt.Println(lang)
 }
 
+func sliceString() {
+	var s string = "apple"
+	var width int = 3
+
+	bytes := []byte(s)
+	sliced := bytes[0:width]
+
+	fmt.Printf("%v\n", string(sliced))
+
+}
+
+func countDigit() {
+
+	var number int = 1234233
+
+	counter := make(map[int]int)
+
+	for number > 0 {
+		digit := number % 10
+		counter[digit]++
+		number /= 10
+	}
+
+	fmt.Println(counter, number)
+}
+
+func firstLetters() {
+	phrase := "apple-orange 3f carrot"
+	var abbr string
+	phraseSplit := strings.Fields(phrase)
+	for _, word := range phraseSplit {
+		runeWordLst := []rune(word)
+		runeWordLst[0] = unicode.ToUpper(runeWordLst[0])
+		if unicode.IsLetter(runeWordLst[0]) {
+			abbr = abbr + string(runeWordLst[0])
+		}
+	}
+	fmt.Println(abbr)
+	// AC
+}
+
+func filter(predicate func(int) bool, iterable []int) []int {
+	var res []int
+	for _, i := range iterable {
+		if predicate(i) {
+			res = append(res, i)
+		}
+	}
+	return res
+}
+
+func filterEven() {
+	var lst []int = []int{1, 2, 3, 4, 5, 6}
+	res := filter(func(i int) bool {
+		if i%2 == 0 {
+			return true
+		} else {
+			return false
+		}
+	}, lst)
+	fmt.Println(res)
+}
+
+func shuffle(nums []int) {
+	rand.Shuffle(len(nums), func(i, j int) { nums[i], nums[j] = nums[j], nums[i] })
+}
+
 func main() {
 	printTime()
 	euclid()
 	concateString()
 	lang()
+	sliceString()
+	countDigit()
+	firstLetters()
+	filterEven()
+	lst := []int{1, 2, 3, 4, 5}
+	shuffle(lst)
+	fmt.Println(lst)
 }
